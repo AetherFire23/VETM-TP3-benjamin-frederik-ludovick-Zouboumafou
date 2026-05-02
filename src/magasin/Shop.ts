4f50fc69725715f7b6bd872ae389ecf20ad41805
f3d <108708015+AetherFire23@users.noreply.github.com>
2026-05-02 10:01:54 -0400

    Ajoute le squelette du magain et du joueur

---
+++ src/magasin/Shop.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+class Shop {
+    // TODO:
+
+    // En tant que le joueur je veux consulter le magasin pour acheter des améliorations.
+    //
+    // En tant que le joueur je veux acheter des améliorations dans le magasin pour l’augmenter nombre de biscuits.
+    //
+    // En tant que le joueur je veux acheter le magasin pour gagner le jeu.
+    getAvailableUpgrades() {
+
+    }
+
+    buyStore() {
+
+    }
+}
