4f8be2ec7f19afba064d7dea490ce4211da956dc
DESKTOP-FRED\fred-pc <108708015+AetherFire23@users.noreply.github.com>
2026-05-07 19:53:02 -0400

    Add css

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
+import "./styles/cookie.css"
 /*
 On load les scripts simplement a partir du import
  */
 import "./buttonClick.ts"
-import "./styles/cookie.css"
-import "./shop/shopHtml/shopDisplay.ts"
+
 /*
 Pour le click handler / observable, il faut sassurer de launcher nos script APRES le script buttonClick pour pas runner no script
 apres l'incremation du coutner.
@@ -14,14 +14,7 @@ apres l'incremation du coutner.
 On load le css a partir du import
  */
 
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML += `
-<section id="center"> 
-    <button id="cookie-button" class="cookie-style"> Cookie </button>
-</section>
-<div id="counter"> 0 </div>
-<div id="shop-div"/>
-<div class="ticks"></div>
-<section id="spacer"></section>
+
 `;
 
--- src/shop/shopHtml/shopDisplay.ts
+++ src/shop/shopHtml/shopDisplay.ts
@@ -4,24 +4,9 @@ import type {IUpgrade} from "../models/IUpgrade.ts";
 
 const show = new Shop();
 
-//
-// window.addEventListener("DOMContentLoaded", () => {
-//     const shopDiv = document.getElementById("shop-div");
-//     if (!shopDiv) throw new DOMException("Null patante blabla");
-//     shopDiv.innerHTML = `
-//     <div class="shop-window">
-//     <div> exxzit </div>
-//     <h1> WElcome to the upgrade shop</h1>
-//     <h2 > Upgrades for almost everyone ;P </h2>
-//     <ul id="shop-list">
-//     </ul>
-//     </div>
-// `;
-//
-//     console.log("sds")
-//     document.getElementById("shop-div")
-//     showUpgradeList();
-// });
+window.addEventListener("DOMContentLoaded", () => {
+    showUpgradeList();
+});
 
 function showUpgradeList() {
     const shopList = document.getElementById("shop-list")!;
