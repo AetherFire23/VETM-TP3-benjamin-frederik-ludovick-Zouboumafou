f2f73df990fd910bd268cba30db6c8f91f214944
2243891 <2243891@cegeplimoilou.ca>
2026-05-11 11:29:38 -0400

    feature : ajout du calcul de cps

--- src/streak/streakCombo.ts
+++ src/streak/streakCombo.ts
@@ -1,11 +1,24 @@
 let clicksThisSecond: number = 0;
-setTimeout(onInterval, 350);
+let arrayOfClicks: number[] = [];
 
-document.addEventListener("click", onClick);
 function onInterval() {
-    console.log("interval")
+    arrayOfClicks.push(clicksThisSecond);
+    clicksThisSecond = 0;
+    console.log();
+    const average = arrayOfClicks.reduce((a, b) => a + b, 0) / arrayOfClicks.length;
+    console.log("Voici le average cps : " + average);
+
+    //Removing the first element if the array is more than 3
+    if (arrayOfClicks.length > 3) {
+        arrayOfClicks.shift();
+    }
 }
+
 function onClick() {
-    console.log("onCLick")
+    clicksThisSecond++;
 }
+
+document.querySelector<HTMLButtonElement>('#cookie-button')!
+    .addEventListener("click", onClick);
+setInterval(onInterval, 1000);
 
