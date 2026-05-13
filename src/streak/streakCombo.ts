713aa0a3b8c09563984c470997651844f8c6af42
2243891 <2243891@cegeplimoilou.ca>
2026-05-13 11:45:17 -0400

    Ajout du Streak Object et du Calculateur de streak

--- src/streak/streakCombo.ts
+++ src/streak/streakCombo.ts
@@ -1,17 +1,38 @@
+import {StreakCalculator} from "./streakCalculator.ts";
+import type {StreakObject} from "./streakCalculator.ts";
+
 let clicksThisSecond: number = 0;
 let arrayOfClicks: number[] = [];
+let currentStreakCookieSum: number = 0;
+
+/***
+ This script calculate the current streak based on cps.
+ If the streaks is at 9 cps you get 1.1x cookies
+ If the streaks is at 10 cps tou get 1.2x cookies
+ If the streaks is at 10.5 cps you get 1.3
+ ***/
 
 function onInterval() {
     arrayOfClicks.push(clicksThisSecond);
     clicksThisSecond = 0;
-    console.log();
     const average = arrayOfClicks.reduce((a, b) => a + b, 0) / arrayOfClicks.length;
     console.log("Voici le average cps : " + average);
 
-    //Removing the first element if the array is more than 3
-    if (arrayOfClicks.length > 3) {
+    //Removing the first element if the array is more than 2
+    if (arrayOfClicks.length > 2) {
         arrayOfClicks.shift();
     }
+}
+
+function calculateStreaks(cps: number) {
+    let currentStreak = StreakCalculator(cps)
+    if(currentStreak != null) {
+        currentStreakCookieSum += cps * currentStreak.streakMultiplier;
+    }
+}
+
+function renderStreak(streakName : string, streakMultiplier: number, currentCookieSum : number) {
+
 }
 
 function onClick() {
